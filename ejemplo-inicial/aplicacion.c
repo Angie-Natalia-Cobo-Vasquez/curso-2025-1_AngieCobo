02b2887febb45690c586d74f1f44748dde0791df
Ubuntu <ubuntu@ip-172-31-3-211.us-east-2.compute.internal>
2025-03-04 19:25:55 +0000

    cambios

---
+++ ejemplo-inicial/aplicacion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Arreglo de strings: char **
+//Funcionamiento del editor nano
+//La funcion main es el entry point (punto de entrada) de un ejecutable de lenguaje c
+int main(void){
+ printf("\nHola mundo!\n");
+ return 0;
+}
